4681ac469ea6568885f9a824e87f867825357114
Barb Donnini <bld2104@gmail.com>
2019-01-23 19:15:48 -0500

    following along with videos and referencing jakes github repo to add caching service worker. install and fetch completed so far

---
+++ sw.js
@@ -0,0 +1,43 @@
+self.addEventLIstener('install', function(event){
+	event.waitUntil(
+		caches.open('restaurant-cache-v1').then(function(cache){
+			return cache.addAll([
+				'./',
+				'./index.html',
+				'./restaurant.html',
+				'./css/styles.css',
+				'./data/restaurants.json',
+				'./js/dbhelper.js',
+				'./js/main.js',
+				'./js/restaurant_info.js',
+				'./img/1.jpg',
+				'./img/2.jpg',
+				'./img/3.jpg',
+				'./img/4.jpg',
+				'./img/5.jpg',
+				'./img/6.jpg',
+				'./img/7.jpg',
+				'./img/8.jpg',
+				'./img/9.jpg',
+				'./img/10.jpg'
+				
+			]);
+		})
+
+		);
+
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    // TODO: remove the old cache
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
